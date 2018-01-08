c620256fa6ab0f2d6f0dfbe9dc0e7b755d58819c
b6008661 <b6008661@hallam.shu.ac.uk>
2018-01-08 13:46:54 +0000

    Changed to php file

---
+++ NHS Project/Christmas Prototype.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<link rel = "stylesheet" type="text/css" href="css/StyleSheet.css">
+	<title> NHS WayFinder </title>
+</head>
+
+
+
+<body>
+<header>
+	<img src="images/nhs_logo.jpg" alt="nhs logo"><a href="https://www.nhs.uk/pages/home.aspx"></a>
+	<form class="locationInput">
+		<input type= "text" name = "location" value="Enter something here ">
+		<input type="submit" value="Submit">
+
+	</form>
+
+
+
+</header>
+
+		<!--<input type = "text" id = "inputbox" value = "">
+		<button onclick = "getName()"> Find Name</button>
+
+		<br>
+		<br>
+		<br>
+	  -->
+		<div class="container">
+		<div class = "box">
+		<h2>Select your starting location:</h2>
+		<div class = "dropDown">
+		<select name = "dropdownboxstart" id= "dropdownboxstart"></select>
+	</div>
+	</div>
+		<br>
+		<br>
+
+		<div class = "box">
+		<h2>Select your destination:</h2>
+		<div class = "dropDown">
+		<select name = "dropdownboxend" id = dropdownboxend></select>
+	</div>
+	</div>
+
+		<br>
+		<br>
+<div class="disabled">
+		<form action="">
+		Disabled? <input type="checkbox" name = "disabled" id="disabled" checked=false>
+		</form>
+	</div>
+
+		<br>
+		<div class="findPath">
+		<button class = "button" onclick = "findPath()"> Find Path </button>
+</div>
+</div>
+</body>
+
+<script src="Script.js"></script>
+
+</html>

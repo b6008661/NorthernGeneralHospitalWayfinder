29c7300159b6626029cb0c270302846206855da8
Anthony Dranfield <34162989+b6018718@users.noreply.github.com>
2018-01-09 16:34:43 +0000

    Updated Script.js
    Script.js  path finding algorithm commented out

--- NHS Project/Christmas_Prototype.php
+++ NHS Project/Christmas_Prototype.php
@@ -2,14 +2,21 @@
 <html>
 
 <head>
-	<!--<meta name ="viewport" content="width-device-width, initial-scale=1";-->
-	<link rel = "stylesheet" type="text/css" href="css/StyleSheet.css">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0,initial-scale=1">
 	<title> NHS WayFinder </title>
+	<link rel = "stylesheet" type="text/css" href="css/StyleSheet.css">
+	<link href="css/desktop.css" media="only screen and (min-width:601px)" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 <header>
 	<img src="images/nhs_logo.jpg" alt="nhs logo"><a href="https://www.nhs.uk/pages/home.aspx"></a>
+<div class="headerText">
+	<h1>Northern General Hospital</h1>
+	<h1>Find your way</h1>
+</div>
+	<form>
 		<input type= "text" name = "location" value="Enter something here ">
 		<input type="submit" value="Search">
 	</form>
@@ -30,28 +37,35 @@
 		<select name = "dropdownboxstart" id= "dropdownboxstart"></select>
 	</div>
 	</div>
+<br>
 		<div class = "box">
 		<h2>Select your destination:</h2>
 		<div class = "dropDown">
 		<select name = "dropdownboxend" id = dropdownboxend></select>
 	</div>
 	</div>
+	<br>
+		<div class = "box">
 		<form action="">
+		<h2>Would you like to avoid staircases?</h2> <input type="checkbox" name = "disabled" checked=false>
 		</form>
+		</div>
 
+		<div class="box">
 		<form action="directions.html">
-<<<<<<< HEAD
+
 			<input id="start" name="start" type="hidden" value="" />
 			<input id="end" name="end" type="hidden" value="" />
 			<input id="disabled" name="disabled" type="hidden" value=""/>
 		<div class="findPath">
 		<button class = "button" onclick = "setValuesForBoxes()"> Find Path </button>
 		</div>
-=======
+
 		<button class = "button" onclick = "findPath()"> Find Path </button>
->>>>>>> 1ae0513b58857eed07f387d8ad0c4398cdd67ac2
+
 		</form>
 		</div>
+
 </div>
 </body>
 

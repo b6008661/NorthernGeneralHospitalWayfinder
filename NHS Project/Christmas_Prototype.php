5f8668301e08ee8b20e3feefe89afe2a68578c02
b6008661 <b6008661@hallam.shu.ac.uk>
2018-01-09 14:08:03 +0000

    Mobile UI redone using flex

--- NHS Project/Christmas_Prototype.php
+++ NHS Project/Christmas_Prototype.php
@@ -2,23 +2,24 @@
 <html>
 
 <head>
-	<!--<meta name ="viewport" content="width-device-width, initial-scale=1";-->
-	<link rel = "stylesheet" type="text/css" href="css/StyleSheet.css">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0,initial-scale=1">
 	<title> NHS WayFinder </title>
+	<link rel = "stylesheet" type="text/css" href="css/StyleSheet.css">
+	<link href="css/desktop.css" media="only screen and (min-width:601px)" rel="stylesheet" type="text/css">
 </head>
-
-
 
 <body>
 <header>
 	<img src="images/nhs_logo.jpg" alt="nhs logo"><a href="https://www.nhs.uk/pages/home.aspx"></a>
-	<form class="locationInput">
+<div class="headerText">
+	<h1>Northern General Hospital</h1>
+	<h1>Find your way</h1>
+</div>
+	<form>
 		<input type= "text" name = "location" value="Enter something here ">
-		<input type="submit" value="Submit">
-
+		<input type="submit" value="Search">
 	</form>
-
-
 
 </header>
 
@@ -36,32 +37,26 @@
 		<select name = "dropdownboxstart" id= "dropdownboxstart"></select>
 	</div>
 	</div>
-		<br>
-		<br>
-
+<br>
 		<div class = "box">
 		<h2>Select your destination:</h2>
 		<div class = "dropDown">
 		<select name = "dropdownboxend" id = dropdownboxend></select>
 	</div>
 	</div>
-
-		<br>
-		<br>
-		<div class = "section">
-			<div class="disabled">
+	<br>
+		<div class = "box">
 		<form action="">
-		<h2>Do you need disabled access? <input type="checkbox" name = "disabled" id="disabled" checked=false>
+		<h2>Would you like to avoid staircases?</h2> <input type="checkbox" name = "disabled" checked=false>
 		</form>
-			</div>
+		</div>
 
-		<br>
+		<div class="box">
 		<form action="directions.html">
-		<div class="findPath">
 		<button class = "button" onclick = "findPath()"> Find Path </button>
-		</div>
 		</form>
 		</div>
+
 </div>
 </body>
 

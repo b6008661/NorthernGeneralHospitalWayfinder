6713e5c7d93fb7d3785175bdbccb74d943b98bd8
b6008661 <b6008661@hallam.shu.ac.uk>
2018-01-10 12:01:35 +0000

    Bootstrap

--- NHS Project/Christmas_Prototype.php
+++ NHS Project/Christmas_Prototype.php
@@ -2,10 +2,12 @@
 <html>
 
 <head>
-	<meta name ="viewport" content="width-device-width, initial-scale=1">
-	<link rel = "stylesheet" type="text/css" href="css/StyleSheet.css">
-	<link href="css/tablet.css" media="only screen and (min-width:700px) and (max-width:1199px)" rel= "stylesheet" type="text/css">
-	<link href="css/desktop.css" media="only screen and (min-width:1200px)" rel="stylesheet" type="text/css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--<link rel = "stylesheet" type="text/css" href="css/StyleSheet.css">-->
+	<!--<link href="css/tablet.css" media="only screen and (min-width:700px) and (max-width:1199px)" rel= "stylesheet" type="text/css">
+	<link href="css/desktop.css" media="only screen and (min-width:1200px)" rel="stylesheet" type="text/css">-->
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link href="css/custom.css" rel="stylesheet" type="text/css">
 	<title> NHS WayFinder </title>
 </head>
 
@@ -16,7 +18,7 @@
 	<h1>Northern General Hospital</h1>
 </div>
 	<form>
-		<input type= "text" name = "location" value="Enter something here ">
+		<input type= "text" name = "location" value="">
 		<input type="submit" value="Search">
 	</form>
 
@@ -29,34 +31,38 @@
 		<br>
 		<br>
 	  -->
-		<div class="container">
-		<div class = "box">
+		<div class="container-fluid">
+		<div class = "row">
+			<div class = "col-sm-3">
+				<div class="box">
 		<h2>Select your starting location:</h2>
 		<div class = "dropDown">
 		<select name = "dropdownboxstart" id= "dropdownboxstart"></select>
 	</div>
 	</div>
-		<div class = "box">
+	</div>
+		<div class = "col-sm-3">
 		<h2>Select your destination:</h2>
 		<div class = "dropDown">
-		<select name = "dropdownboxend" id = dropdownboxend></select>
+		<select name = "dropdownboxend" id = "dropdownboxend"></select>
 	</div>
 	</div>
 
-<div class="box">
-
+<div class="col-sm-3">
 <div class="findPath">
 		<form action="directions.html">
 			<input id="start" name="start" type="hidden" value="" />
 			<input id="end" name="end" type="hidden" value="" />
 			<input id="disabled" name="disabled" type="hidden" value=""/>
-
+</div>
 		<!--<button class = "button" onclick = "setValuesForBoxes()"> Find Path </button>-->
-
+<div class="col-md-4">
 		<button class = "button" onclick = "findPath()"> Find Path </button>
 		</form>
 		</div>
 	</div>
+</div>
+</div>
 </div>
 </body>
 
